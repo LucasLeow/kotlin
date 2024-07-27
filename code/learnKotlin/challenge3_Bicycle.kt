9d48d715b0c3e090053cb6c7edb5bf1f51e55ff7
Lucas Leow <leowjianhoong@gmail.com>
2024-07-27 13:36:03 +0800

    Complete challenge 3 Bicycle superclass

---
+++ code/learnKotlin/challenge3_Bicycle.kt
@@ -0,0 +1,12 @@
+package learnKotlin
+
+class Bicycle(var cadence: Int, var speed: Int, var gear: Int) {
+
+    fun applyBrake(decrement: Int) {
+        speed -= decrement
+    }
+
+    fun speedUp(increment: Int) {
+        speed += increment
+    }
+}
