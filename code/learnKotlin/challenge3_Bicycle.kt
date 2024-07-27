7243586cf5c94e840efb28c8df19fd7da73ae71a
Lucas Leow <leowjianhoong@gmail.com>
2024-07-27 13:43:04 +0800

    Complete challenge 3 Mountain Bike subclass

--- code/learnKotlin/challenge3_Bicycle.kt
+++ code/learnKotlin/challenge3_Bicycle.kt
@@ -1,6 +1,6 @@
 package learnKotlin
 
-class Bicycle(var cadence: Int, var speed: Int, var gear: Int) {
+open class Bicycle(var cadence: Int, var speed: Int, var gear: Int) {
 
     fun applyBrake(decrement: Int) {
         speed -= decrement
---
+++ code/learnKotlin/challenge3_MountainBike.kt
@@ -0,0 +1,5 @@
+package learnKotlin
+
+class MountainBike(var seatHeight: Int, cadence: Int, speed: Int, gear: Int): Bicycle(cadence, speed, gear) {
+
+}
